34525c21569bb5b4a153d8881612c8baf4bd69d1
bori00 <fazakasbori@gmail.com>
2020-07-16 08:15:13 +0000

    add videos to cards in modal

--- portfolio/src/main/webapp/quiz_alert.js
+++ portfolio/src/main/webapp/quiz_alert.js
@@ -1,13 +1,15 @@
-//alert("Welcome!\nThis page contains some quiz questions. Hover your mouse over the question boc to find out the answer.\nGood luck!");
 var modal = document.getElementById("modal-quiz");
 console.log(modal);
+
 window.onload = function() {
     modal.style.display = "block";
 }
-var span = document.getElementsByClassName("close")[0];
+
+var span = document.getElementById("close-quiz");
 span.onclick = function() {
   modal.style.display = "none";
 }
+
 window.onclick = function(event) {
   if (event.target == modal) {
     modal.style.display = "none";
