c16b45c7b630c222a41e1546b2a149487cd8ae0d
bori00 <fazakasbori@gmail.com>
2020-07-20 16:02:48 +0000

    display comments fetched from server on comments page

--- portfolio/src/main/webapp/comments.js
+++ portfolio/src/main/webapp/comments.js
@@ -1,12 +1,32 @@
-function writeCommentsToConsole(){
+function displayComments(){
     fetch('/comments-data')
-    .catch(console.log('failed to fetch comments data from server'))
+    .catch(error => console.log('failed to fetch comments data from server'))
     .then(response => response.json())
-    .catch(console.log('failed to parse comments'))
-    .then(response => {
-        for(var i=0; i<response.length; i++){
-            console.log(response[i]);
-        }
-    })
-    .catch(console.log('failed to log comments to console'));
+    .catch(error => console.log('failed to parse comments'))
+    .then(response => addAllCommentsToDOM(response))
+    .catch(error => console.log('failed to print comments to DOM'));
+}
+
+function addCommentToDOM(comment){
+    console.log("displaying comment " + comment)
+    //create container
+    var card_holder = document.createElement("div");
+    card_holder.setAttribute('class', 'card-holder-small');
+    var card = document.createElement("div");
+    card.setAttribute('class', 'card-small');
+    card_holder.appendChild(card);
+    //add comment to container
+    var para = document.createElement("p"); 
+    para.innerText = comment;
+    card.appendChild(para);  
+    //add container to main 
+    var main = document.getElementById("main-comments");        
+    main.appendChild(card_holder);  
+}
+
+function addAllCommentsToDOM(comments){
+    console.log("started adding comments to DOM")
+    for(var i=0; i<comments.length; i++){
+        addCommentToDOM(comments[i]);
+    }
 }
