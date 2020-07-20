4d63074e88a146c17b34eeddf26ccb3821194a96
bori00 <fazakasbori@gmail.com>
2020-07-20 08:58:13 +0000

    add catch statements

--- portfolio/src/main/webapp/welcome_data.js
+++ portfolio/src/main/webapp/welcome_data.js
@@ -1,9 +1,9 @@
 function getWelcomeText(){
     console.log("trying to get welcome text");
     fetch('/welcome-data')
-    //.catch(console.log('failed to get resposne'))
+    .catch(console.log('failed to get resposne'))
     .then(response => response.text())
-    //.catch(console.log('failed to convert to text'))
+    .catch(console.log('failed to convert to text'))
     .then(welcomeText => document.getElementById('welcome-data').innerText=welcomeText)
-    //.catch(console.log('failed to print text to html element'));
+    .catch(console.log('failed to print text to html element'));
 }
