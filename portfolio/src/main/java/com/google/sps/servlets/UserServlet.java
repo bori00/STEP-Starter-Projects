fb0b6e52d50609dbdf42d7b75ffbcdbaae134858
bori00 <fazakasbori@gmail.com>
2020-07-24 11:18:30 +0000

    check if user is logged in

--- portfolio/src/main/java/com/google/sps/servlets/UserServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/UserServlet.java
@@ -19,14 +19,26 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import com.google.appengine.api.users.UserService;
+import com.google.appengine.api.users.UserServiceFactory;
+import com.google.gson.Gson;
 
 
 /** This servlet handles user's data: doGet() checks if the user is authenticated 
  * and redirects them to a log in page if needed*/
-@WebServlet("/comments-data")
-public class CommentsServlet extends HttpServlet {
+@WebServlet("/user-data")
+public class UserServlet extends HttpServlet {
     @Override
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException{ 
-        
+        UserService userService = UserServiceFactory.getUserService();
+        response.setContentType("application/json;");
+        boolean isUserLoggedIn = userService.isUserLoggedIn();
+        response.getWriter().println(convertToJsonUsingGson(isUserLoggedIn));
+    }
+
+    private String convertToJsonUsingGson(Object o) {
+        Gson gson = new Gson();
+        String json = gson.toJson(o);
+        return json;
     }
 }
