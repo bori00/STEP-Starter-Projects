9fb8e786dcccdc7fec64e23bae60711378aee90f
bori00 <fazakasbori@gmail.com>
2020-07-27 10:24:56 +0000

    add wantToVisit places to map with blue markers

--- portfolio/src/main/webapp/maps.js
+++ portfolio/src/main/webapp/maps.js
@@ -35,5 +35,17 @@ function createMap() {
     const map = new google.maps.Map(
         document.getElementById('map'), myMapOptions); 
     cluj.addMarkerToMap(map);
+    addWantToVisitPlacesToMap(map);
+}
+
+
+function addWantToVisitPlacesToMap(map){
+    var wantToVisitIcon = "paddle/blu-blank.png";
+    var wantToVisitPlaces = [
+        new Place(61.3410, 28.0110, "Finnish Lakeland", "", wantToVisitIcon),
+        new Place(49.3802, 10.1867, "Rothenburg ob der Tauber", "", wantToVisitIcon),
+        new Place(44.5994, 2.3960, "Conques", "", wantToVisitIcon),
+    ];
+    wantToVisitPlaces.forEach(place => place.addMarkerToMap(map));
 }
 
