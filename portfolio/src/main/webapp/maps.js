8d5854c2f2612521e7781274229ff7160c35192e
bori00 <fazakasbori@gmail.com>
2020-07-27 09:42:34 +0000

    create place class

--- portfolio/src/main/webapp/maps.js
+++ portfolio/src/main/webapp/maps.js
@@ -1,16 +1,31 @@
+class Place{
+    constructor(positionLat, positionLng, title, label, markerColor){
+        this.position = new google.maps.LatLng(positionLat, positionLng);
+        this.title = title;
+        this.marker = new google.maps.Marker({position: this.position, title: this.title, label: label});
+        console.log(this.marker);
+    }
+
+    addMarkerToMap(map){
+        console.log("Add " + this.title + "to map");
+        this.marker.setMap(map);
+    }
+
+    get getPosition(){
+        return this.position;
+    }
+}
+
 function createMap() {
-    var clujLatLng = new google.maps.LatLng(46.7712, 23.6236); //Cluj-Napoca coordinates
+    var cluj = new Place(46.7712, 23.6236, "My hometown: Cluj-Napoca", "M", "green"); //Cluj-Napoca coordinates
     var myMapOptions = {
         zoom: 8,
-        center: clujLatLng,
+        center: cluj.getPosition,
         mapTypeId: 'roadmap',
-        backgroundColor: "#8E8D8A"
+        backgroundColor: "#8E8D8A",
     };
     const map = new google.maps.Map(
         document.getElementById('map'), myMapOptions); 
-    addMarker(map, clujLatLng, "My hometown: Cluj");
+    cluj.addMarkerToMap(map);
 }
 
-function addMarker(map, markerPosition, markerTitle){
-    var marker = new google.maps.Marker({position: markerPosition, map: map, title: markerTitle});
-}
