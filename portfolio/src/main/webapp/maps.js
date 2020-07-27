834c2b8102ee631c1b35a3d66c3e9c25e4fd1076
bori00 <fazakasbori@gmail.com>
2020-07-27 07:17:14 +0000

    add background color to map

--- portfolio/src/main/webapp/maps.js
+++ portfolio/src/main/webapp/maps.js
@@ -1,5 +1,7 @@
 function createMap() {
   const map = new google.maps.Map(
-      document.getElementById('map'),
-      {center: {lat: 46.7712, lng: 23.6236}, zoom: 8}); //Cluj-Napoca coordinates
+      document.getElementById('map'),{
+        center: {lat: 46.7712, lng: 23.6236},
+        zoom: 8,
+        backgroundColor: "#8E8D8A"}); //Cluj-Napoca coordinates
 }
