2820a5d8efea6d8b03e9e6567ba5550fbc087186
bori00 <fazakasbori@gmail.com>
2020-07-27 09:48:26 +0000

    add _ notation to fields that should be private

--- portfolio/src/main/webapp/maps.js
+++ portfolio/src/main/webapp/maps.js
@@ -1,18 +1,18 @@
 class Place{
     constructor(positionLat, positionLng, title, label, markerColor){
-        this.position = new google.maps.LatLng(positionLat, positionLng);
-        this.title = title;
-        this.marker = new google.maps.Marker({position: this.position, title: this.title, label: label});
-        console.log(this.marker);
+        this._position = new google.maps.LatLng(positionLat, positionLng);
+        this._title = title;
+        this._marker = new google.maps.Marker({position: this._position, title: this._title, label: label});
+        console.log(this._marker);
     }
 
     addMarkerToMap(map){
-        console.log("Add " + this.title + "to map");
-        this.marker.setMap(map);
+        console.log("Add " + this._title + "to map");
+        this._marker.setMap(map);
     }
 
-    get getPosition(){
-        return this.position;
+    get position(){
+        return this._position;
     }
 }
 
@@ -20,7 +20,7 @@ function createMap() {
     var cluj = new Place(46.7712, 23.6236, "My hometown: Cluj-Napoca", "M", "green"); //Cluj-Napoca coordinates
     var myMapOptions = {
         zoom: 8,
-        center: cluj.getPosition,
+        center: cluj.position,
         mapTypeId: 'roadmap',
         backgroundColor: "#8E8D8A",
     };
