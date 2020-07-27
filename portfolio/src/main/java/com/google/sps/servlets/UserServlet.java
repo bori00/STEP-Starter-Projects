98932529eab6be14d0531c8ec97387812e4c2e08
bori00 <fazakasbori@gmail.com>
2020-07-27 13:25:57 +0000

    format javadoc for UserServlet

--- portfolio/src/main/java/com/google/sps/servlets/UserServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/UserServlet.java
@@ -33,8 +33,11 @@ import com.google.appengine.api.datastore.FetchOptions;
 import java.util.List; 
 
 
-/** This servlet handles user's data: {@link #doGet()} checks if the user is 
-authenticated and redirects them to a log in/log out page if needed. */
+/** 
+* This servlet handles user's data.
+*
+* <p> The {@link #doGet(HttpServletRequest, HttpServletResponse) doGet()} checks if the user is 
+* authenticated and redirects them to a log in/log out page if needed. */
 @WebServlet("/user-data")
 public class UserServlet extends HttpServlet {
 
