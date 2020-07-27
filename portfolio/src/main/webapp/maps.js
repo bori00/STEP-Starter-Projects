d61f7adb446e578e3dfac789038a4b2ba38ad072
bori00 <fazakasbori@gmail.com>
2020-07-27 08:04:34 +0000

    add marker to my hometown

--- portfolio/src/main/webapp/maps.js
+++ portfolio/src/main/webapp/maps.js
@@ -1,11 +1,16 @@
 function createMap() {
-    var clujLatlng = new google.maps.LatLng(46.7712, 23.6236); //Cluj-Napoca coordinates
+    var clujLatLng = new google.maps.LatLng(46.7712, 23.6236); //Cluj-Napoca coordinates
     var myMapOptions = {
         zoom: 8,
-        center: clujLatlng,
+        center: clujLatLng,
         mapTypeId: 'roadmap',
         backgroundColor: "#8E8D8A"
     };
     const map = new google.maps.Map(
         document.getElementById('map'), myMapOptions); 
+    addMarker(map, clujLatLng);
+}
+
+function addMarker(map, markerPosition){
+    var marker = new google.maps.Marker({position: markerPosition, map: map});
 }
