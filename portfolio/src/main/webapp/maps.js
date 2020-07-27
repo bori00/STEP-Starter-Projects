f046a91c2c62ba3aedd5b437a0e5286b5f6850e5
bori00 <fazakasbori@gmail.com>
2020-07-27 11:05:34 +0000

    create marker types wantToVisit, favorite and important with different icons

--- portfolio/src/main/webapp/maps.js
+++ portfolio/src/main/webapp/maps.js
@@ -1,17 +1,31 @@
+var iconBase = 'https://maps.google.com/mapfiles/kml/';
+const wantToVisitIcon = 'wantToVisit';
+const favoriteIcon = 'favorite';
+const importantIcon = 'important';
+var icons = {
+    wantToVisit: {
+        icon: iconBase + 'paddle/blu-blank.png'
+    },
+    favorite: {
+        icon: iconBase + "paddle/ylw-blank.png",
+    },
+    important: {
+        icon: iconBase + 'paddle/orange-blank.png'
+    }
+};
+
 class Place{
-    constructor(positionLat, positionLng, title, label, iconName){
+    constructor(positionLat, positionLng, title, label, iconType){
         this._position = new google.maps.LatLng(positionLat, positionLng);
         this._title = title;
-        var iconBase = 'https://maps.google.com/mapfiles/kml/';
         this._marker = new google.maps.Marker({
             position: this._position, 
             title: this._title, 
             label: label, 
             icon: { 
-                url: iconBase + iconName,
+                url: icons[iconType].icon
             }
         });
-        console.log(this._marker);
     }
 
     addMarkerToMap(map){
@@ -25,7 +39,7 @@ class Place{
 }
 
 function createMap() {
-    var cluj = new Place(46.7712, 23.6236, "My hometown: Cluj-Napoca", "M", "paddle/grn-blank.png"); //Cluj-Napoca coordinates
+    var cluj = new Place(46.7712, 23.6236, "My hometown: Cluj-Napoca", "M", importantIcon); //Cluj-Napoca coordinates
     var myMapOptions = {
         zoom: 8,
         center: cluj.position,
@@ -36,16 +50,26 @@ function createMap() {
         document.getElementById('map'), myMapOptions); 
     cluj.addMarkerToMap(map);
     addWantToVisitPlacesToMap(map);
+    addFavoritePlacesToMap(map);
 }
 
 
 function addWantToVisitPlacesToMap(map){
-    var wantToVisitIcon = "paddle/blu-blank.png";
     var wantToVisitPlaces = [
-        new Place(61.3410, 28.0110, "Finnish Lakeland", "", wantToVisitIcon),
-        new Place(49.3802, 10.1867, "Rothenburg ob der Tauber", "", wantToVisitIcon),
-        new Place(44.5994, 2.3960, "Conques", "", wantToVisitIcon),
+        new Place(61.3410, 28.0110, "Finnish Lakeland", "1", wantToVisitIcon),
+        new Place(49.3802, 10.1867, "Rothenburg ob der Tauber", "2", wantToVisitIcon),
+        new Place(44.5994, 2.3960, "Conques", "3", wantToVisitIcon),
     ];
     wantToVisitPlaces.forEach(place => place.addMarkerToMap(map));
+}
+
+function addFavoritePlacesToMap(map){
+    var favoritePlaces = [
+        new Place(37.1761, -3.5881, "Granada", "1", favoriteIcon),
+        new Place(46.7753, 23.5817, "Fellegvar, Cluj", "2", favoriteIcon),
+        new Place(47.5576, 10.7498, "Neuschwanstein Castle", "3", favoriteIcon),
+        new Place(52.4729, 4.8219, "Zaanse Schans", "4", favoriteIcon),
+    ];
+    favoritePlaces.forEach(place => place.addMarkerToMap(map));
 }
 
