9303d0c80bd5f2d204e44f45d4516a8c7016eb3c
bori00 <fazakasbori@gmail.com>
2020-07-27 19:09:56 +0000

    set user's is from userService to be the key in datastore

--- portfolio/src/main/java/com/google/sps/user/repository/impl/DatastoreUserRepository.java
+++ portfolio/src/main/java/com/google/sps/user/repository/impl/DatastoreUserRepository.java
@@ -11,11 +11,13 @@ import com.google.appengine.api.datastore.Entity;
 import com.google.appengine.api.datastore.PreparedQuery;
 import com.google.appengine.api.datastore.Query;
 import com.google.appengine.api.datastore.FetchOptions;
+import com.google.appengine.api.datastore.Key;
+import com.google.appengine.api.datastore.KeyFactory;
+import com.google.appengine.api.datastore.EntityNotFoundException;
 import java.util.List; 
 
 public class DatastoreUserRepository implements UserRepository{
     public static final String ENTITY_NAME = "User";
-    public static final String ID_PROPERTY = "userId";
     public static final String FIRST_NAME_PROPERTY = "firstName";
     public static final String LAST_NAME_PROPERTY = "lastName";
     public static final String PHONE_PROPERTY = "phone";
@@ -29,8 +31,7 @@ public class DatastoreUserRepository implements UserRepository{
     }
 
     private Entity getUserEntity(User user){
-        Entity userEntity = new Entity(ENTITY_NAME);
-        userEntity.setProperty(ID_PROPERTY, user.getId());
+        Entity userEntity = new Entity(ENTITY_NAME, user.getId());
         userEntity.setProperty(FIRST_NAME_PROPERTY, user.getFirstName());
         userEntity.setProperty(LAST_NAME_PROPERTY, user.getLastName());
         userEntity.setProperty(EMAIL_PROPERTY, user.getEmail());
@@ -41,31 +42,48 @@ public class DatastoreUserRepository implements UserRepository{
 
     @Override
     public boolean isUserDataSaved(String id){
-        Query commentsQuery = new Query(ENTITY_NAME).setFilter(new Query.FilterPredicate(ID_PROPERTY, Query.FilterOperator.EQUAL, id));
+        /*Query commentsQuery = new Query(ENTITY_NAME).setFilter(new Query.FilterPredicate(ID_PROPERTY, Query.FilterOperator.EQUAL, id));
         DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
         List<Entity> results = datastore.prepare(commentsQuery).asList(FetchOptions.Builder.withDefaults());
         if(results.size()>0) return true;
-        else return false;
+        else return false;*/
+        DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+        Key userKey = KeyFactory.createKey(ENTITY_NAME, id);
+        try {
+            getUserFromUserEntity(datastore.get(userKey));
+            return true;
+        }
+        catch(EntityNotFoundException e){
+            return false;
+        }
     }
 
     @Override @Nullable
     public User getUser(String id){
-        return getUserFromUserEntity(getSavedUserEntity(id));
+        //return getUserFromUserEntity(getSavedUserEntity(id));
+        DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+        Key userKey = KeyFactory.createKey(ENTITY_NAME, id);
+        try {
+            return getUserFromUserEntity(datastore.get(userKey));
+        }
+        catch(EntityNotFoundException e){
+            return null;
+        }
     }
 
-    @Nullable
+    /*@Nullable
     private Entity getSavedUserEntity(String id){
         Query commentsQuery = new Query(ENTITY_NAME).setFilter(new Query.FilterPredicate(ID_PROPERTY, Query.FilterOperator.EQUAL, id));
         DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
         List<Entity> results = datastore.prepare(commentsQuery).asList(FetchOptions.Builder.withDefaults());
         if(results.size()>0) return results.get(0);
         else return null;
-    }
+    }*/
 
     @Nullable
     private User getUserFromUserEntity(Entity userEntity){
         if(userEntity==null) return null;
-        String id = (String) userEntity.getProperty(ID_PROPERTY);
+        String id = (String) userEntity.getKey().getName();
         String firstName = (String) userEntity.getProperty(FIRST_NAME_PROPERTY);
         String lastName = (String) userEntity.getProperty(LAST_NAME_PROPERTY);
         String email = (String) userEntity.getProperty(EMAIL_PROPERTY);
