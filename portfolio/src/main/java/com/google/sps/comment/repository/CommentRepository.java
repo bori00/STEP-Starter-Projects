9a0489e9f48efb402a9f823661e33a68b7eecb0e
bori00 <fazakasbori@gmail.com>
2020-07-28 18:03:31 +0000

    add javadoc for non-trivial methods

--- portfolio/src/main/java/com/google/sps/comment/repository/CommentRepository.java
+++ portfolio/src/main/java/com/google/sps/comment/repository/CommentRepository.java
@@ -8,8 +8,13 @@ import java.util.List;
 public interface CommentRepository {
     public void saveComment(Comment comment);
 
+    /* Returns all comments from the database. */
     public List<Comment> getAllComments();
 
+    /* 
+    * Returns maxNoComments, if there are available at least maxNoComments comments in the database. 
+    * Otherwise returns all comments.
+    */
     public List<Comment> getGivenNumberOfComments(int maxNoComments);
 
     public void deleteAllComments();
