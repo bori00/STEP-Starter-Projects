6af4171c4aebed8717a59b2b4b96e9a2cfbb3e07
bori00 <fazakasbori@gmail.com>
2020-07-29 07:13:09 +0000

    add whitespaces

--- portfolio/src/main/webapp/maps.js
+++ portfolio/src/main/webapp/maps.js
@@ -14,11 +14,11 @@ var icons = {
     }
 };
 
-class Place{
-    constructor(positionLat, positionLng, title, label, iconType, description){
+class Place {
+    constructor(positionLat, positionLng, title, label, iconType, description) {
         this._position = new google.maps.LatLng(positionLat, positionLng);
         this._title = title;
-        this._marker = new google.maps.Marker({
+        this._marker = new google.maps.Marker( {
             position: this._position, 
             title: this._title, 
             label: label, 
@@ -26,16 +26,16 @@ class Place{
                 url: icons[iconType].icon
             }
         });
-        if (description !== ""){
+        if (description !== "") {
             console.log("add infoWindow to marker: " + this._title);
             this._infoWindow = new google.maps.InfoWindow({content: description});
         }
     }
 
-    addMarkerToMap(map){
+    addMarkerToMap(map) {
         console.log("Add " + this._title + "to map");
         this._marker.setMap(map);
-        if(this._infoWindow !== undefined){
+        if(this._infoWindow !== undefined) {
             this._marker.addListener('click', () => {
                 this._infoWindow.open(map, this._marker);
                 console.log("show infoWindow of marker: " + this._title);
@@ -43,14 +43,14 @@ class Place{
         }
     }
 
-    get position(){
+    get position() {
         return this._position;
     }
 }
 
 function createMap() {
-    var cluj = new Place(46.7712, 23.6236, "My hometown: Cluj-Napoca", "M", 
-                        importantIcon, "<h3>Cluj-Napoca</h3> This is my hometown, the heart of Transylvania"); 
+    var cluj = new Place(46.7712, 23.6236, "My hometown: Cluj-Napoca", "M", importantIcon, 
+                        "<h3>Cluj-Napoca</h3> This is my hometown, the heart of Transylvania"); 
     var myMapOptions = {
         zoom: 8,
         center: cluj.position,
@@ -65,18 +65,18 @@ function createMap() {
 }
 
 
-function addWantToVisitPlacesToMap(map){
+function addWantToVisitPlacesToMap(map) {
     var wantToVisitPlaces = [
         new Place(61.3410, 28.0110, "Finnish Lakeland", "1", wantToVisitIcon, 
-                    "<h3>Finnish Lakeland</h3> is a blue labyrinth of lakes, islands, rivers and canals, interspersed with forests and ridges, stretching for hundreds of kilometres in a placid and staggeringly beautiful expanse."),
+                "<h3>Finnish Lakeland</h3> is a blue labyrinth of lakes, islands, rivers and canals, interspersed with forests and ridges, stretching for hundreds of kilometres in a placid and staggeringly beautiful expanse."),
         new Place(49.3802, 10.1867, "Rothenburg ob der Tauber", "2", wantToVisitIcon, 
-        "<h3>Rothenburg ob der Tauber</h3> is a German town in northern Bavaria known for its medieval architecture. Half-timbered houses line the cobblestone lanes of its old town."),
+                "<h3>Rothenburg ob der Tauber</h3> is a German town in northern Bavaria known for its medieval architecture. Half-timbered houses line the cobblestone lanes of its old town."),
         new Place(44.5994, 2.3960, "Conques", "3", wantToVisitIcon, ""),
     ];
     wantToVisitPlaces.forEach(place => place.addMarkerToMap(map));
 }
 
-function addFavoritePlacesToMap(map){
+function addFavoritePlacesToMap(map) {
     var favoritePlaces = [
         new Place(37.1761, -3.5881, "Granada", "1", favoriteIcon, ""),
         new Place(46.7753, 23.5817, "Fellegvar, Cluj", "2", favoriteIcon, ""),
