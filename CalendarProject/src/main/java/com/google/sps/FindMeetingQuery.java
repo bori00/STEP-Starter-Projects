b2107645376725f0bb06c49e104e445cca7c46fe
bori00 <fazakasbori@gmail.com>
2020-07-30 10:45:04 +0000

    get list of timeranges and sort it based on starting times

--- CalendarProject/src/main/java/com/google/sps/FindMeetingQuery.java
+++ CalendarProject/src/main/java/com/google/sps/FindMeetingQuery.java
@@ -15,26 +15,44 @@
 package com.google.sps;
 
 import java.util.Collection;
-import java.util.Set;
-import java.util.Iterator;
+import java.util.Collections;
 import java.util.HashSet;
+import java.util.Comparator;
+import java.util.Iterator;
+import java.util.ArrayList;
+import java.util.List;
+
 
 public final class FindMeetingQuery {
     public Collection<TimeRange> query(Collection<Event> events, MeetingRequest request) {
         //throw new UnsupportedOperationException("TODO: Implement this method.");
+        System.out.println("Size of events is: " + events.size());
         events.removeIf(event -> !existCommonAttendees(event.getAttendees(), request.getAttendees()));
+        System.out.println("Size of events after removal is: " + events.size());
+        List<TimeRange> unavailableTimes = getListOfTimeRanges(events);
+        Collections.sort(unavailableTimes, TimeRange.ORDER_BY_START);
+        System.out.println("the ordered timeranges are: " + unavailableTimes);
+        return unavailableTimes;
+    }
+
+    private List<TimeRange> getListOfTimeRanges(Collection<Event> events) {
+        List<TimeRange> result = new ArrayList<>();
+        for (Event event : events) {
+            result.add(event.getWhen());
+        }
+        return result;
     }
 
     /* 
     * Checks if there is exists an intersection(a common attendee) of the attendee list of two events
     */
-    private boolean existCommonAttendees(Set<String> attendeesEventA, Collection<String> attendeesEventB) {
-        //swap hashSets so that I can iterate through the shorter one
-        /*if (attendeesEventA.size()>attendeesEventB.size()) { 
-            Set<String> helper = attendeesEventA;
+    private boolean existCommonAttendees(Collection<String> attendeesEventA, Collection<String> attendeesEventB) {
+        //swap collections so that I can iterate through the shorter one
+        if (attendeesEventA.size()>attendeesEventB.size()) { 
+            Collection<String> helper = attendeesEventA;
             attendeesEventA = attendeesEventB;
             attendeesEventB = helper;
-        }*/
+        }
 
         //itarate through the attendees of event A and check if any of them also attends event B. If so,return true
         Iterator iteratorB = attendeesEventB.iterator(); 
